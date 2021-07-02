77fd1665e598f3277748287bfe561c67d581c6f6
Pritom Paul <79314347+Pritomprasantpaul@users.noreply.github.com>
2021-07-02 11:33:49 +0530

    Delete productmangment directory

--- productmangment/src/main/java/com/te/empwebapp/service/EmployeeService.java
+++
@@ -1,8 +0,0 @@
-package com.te.empwebapp.service;
-
-import com.te.empwebapp.beans.EmployeeInfoBean;
-
-public interface EmployeeService {
-
-	public EmployeeInfoBean authenticate(int id, String pwd);
-}
--- productmangment/src/main/webapp/index.jsp
+++
@@ -1,5 +0,0 @@
-<html>
-<body>
-<h2>Employee Managment System</h2>
-</body>
-</html>
